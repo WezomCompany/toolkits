d19120d60d54d5a9654a47753f289cf1c8ff2462
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2021-02-12 23:42:25 +0200

    [all] Remove GitHub Workflow Tests

--- packages/math/src/__spec__/sum.spec.ts
+++ packages/math/src/__spec__/sum.spec.ts
@@ -1,5 +1,5 @@
 import { mathSum } from '../index';
-import jestFunctionSignatureTest from '../../../jest-cjs/dist/function-signature-test';
+import { jestFunctionSignatureTest } from '@wezom/toolkit-jest-cjs';
 
 describe('Function signature should match specification', () => {
 	jestFunctionSignatureTest(mathSum, [
