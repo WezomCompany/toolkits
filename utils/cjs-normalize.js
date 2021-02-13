32353c7b5caecc82b3da02079f874457589d65ff
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2021-02-13 16:46:53 +0200

    [all] Fix CommonJS normalization

---
+++ utils/cjs-normalize.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const fromCWD = require('from-cwd');
+const glob = require('glob');
+
+const regExp = /'@wezom\/toolkit-(array|css-in-js|math|promise)'/g;
+
+function normalizeImports(name) {
+	console.log(`Normalize @wezom/toolkit-${name}-cjs`);
+	glob.sync(`packages/${name}-cjs/dist/**/*{.d.ts,.js}`).forEach((file) => {
+		const content = fs.readFileSync(file).toString();
+		if (regExp.test(content)) {
+			fs.writeFileSync(
+				file,
+				content.replace(regExp, (str, name) => `'@wezom\/toolkit-${name}-cjs'`)
+			);
+		}
+	});
+}
+
+normalizeImports('array');
+normalizeImports('css-in-js');
+normalizeImports('math');
+normalizeImports('promise');
