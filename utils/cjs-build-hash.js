94cf294a4f205a3b099b1114487e5e7085549fa9
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2021-03-01 14:00:03 +0200

    clean-up

--- utils/cjs-build-hash.js
+++ utils/cjs-build-hash.js
@@ -5,23 +5,6 @@ const crypto = require('crypto');
 const argv = require('yargs').argv;
 
 /**
- * @param {string} str
- * @return {number}
- */
-function hashCode(str) {
-	var hash = 0,
-		i,
-		chr;
-	if (str.length === 0) return hash;
-	for (i = 0; i < str.length; i++) {
-		chr = str.charCodeAt(i);
-		hash = (hash << 5) - hash + chr;
-		hash |= 0; // Convert to 32bit integer
-	}
-	return hash;
-}
-
-/**
  * @param {string} pattern
  * @param {string} hashFile
  * @returns {void}
