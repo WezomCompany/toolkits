402970a44462b9fc7213cf4d44c3961390589828
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2021-03-07 17:09:43 +0200

    [toolkit-array] Adds new util `arrayFilterUnique`

--- packages/array/src/index.ts
+++ packages/array/src/index.ts
@@ -1,5 +1,6 @@
 export { default as arrayClone } from './clone';
 export { default as arrayFilterNullable } from './filter-nullable';
+export { default as arrayFilterUnique } from './filter-unique';
 export { default as arrayAddUnique } from './add-unique';
 export { default as arrayRemoveItems } from './remove-items';
 export { default as arraySimpleRandomFn } from './simple-random-fn';
