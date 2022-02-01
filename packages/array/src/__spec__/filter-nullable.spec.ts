47238fa8ef2fa082971ee2897b85f2cb3df232cd
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2022-02-01 23:47:30 +0200

    [array] Adds custom predicate to `arrayFilterNullable` method

--- packages/array/src/__spec__/filter-nullable.spec.ts
+++ packages/array/src/__spec__/filter-nullable.spec.ts
@@ -21,3 +21,38 @@ describe('Function signature should match specification', () => {
 		}
 	]);
 });
+
+interface A {
+	x: string;
+}
+
+interface B {
+	x: string;
+	y: string;
+}
+
+interface C {
+	x: string;
+	y: number;
+}
+type D = Partial<C>;
+
+describe('Custom predicate', () => {
+	test('A to B', () => {
+		const data: (A | B | null)[] | null = [{ x: '1' }, { x: '2' }, null, { x: '3', y: '4' }];
+		const result = arrayFilterNullable(
+			data,
+			(item): item is B => item != null && 'y' in item
+		);
+		expect(result).toStrictEqual([{ x: '3', y: '4' }]);
+	});
+
+	test('C to D', () => {
+		const data: (C | D | null)[] | null  = [{ y: 1, x: '2' }, null, { x: '2' }, { y: 4 }];
+		const result = arrayFilterNullable(
+			data,
+			(item): item is D => item != null && item.y != null && item.x != null
+		);
+		expect(result).toStrictEqual([{ y: 1, x: '2' }]);
+	});
+});
--- packages/array/src/filter-nullable.ts
+++ packages/array/src/filter-nullable.ts
@@ -5,10 +5,36 @@
  *  arrayFilterNullable([1, null, 2, undefined, 3, false, 0]); // => [1, 2, 3, false, 0]
  *  arrayFilterNullable(null); // => []
  *  arrayFilterNullable(); // => []
+ *
+ *  // Custom predicate. Use case example #1
+ *  interface A { x: string };
+ *  interface B { x: string; y: string };
+ *  const data: (A | B | null)[] | null = [{ x: '1' }, { x: '2' }, null, { x: '3', y: '4' }];
+ * 	const result = arrayFilterNullable(
+ * 		data,
+ * 		(item): item is B => item != null && 'y' in item
+ * 	);
+ *
+ *  // Custom predicate. Use case example #3
+ *  interface D { x: number; y: string };
+ *  type E = Partial<D>;
+ *  const data: (C | D | null)[] | null = [{ y: 1, x: '2' }, null, { x: '2' }, { y: 4 }];
+ * 	const result = arrayFilterNullable(
+ * 		data,
+ * 		(item): item is D => item != null && item.y != null && item.x !== null
+ * 	);
  */
-export default function <T>(data?: (T | null)[] | null): T[] {
+export default function <T, R extends T = T>(
+	data?: (T | null | undefined)[] | null,
+	predicate: (
+		value: T | null | undefined,
+		index: number,
+		array: (T | null | undefined)[]
+	) => value is R = (item): item is R => item != null
+): R[] {
 	if (data == null || !Array.isArray(data)) {
 		return [];
+	} else {
+		return data.filter(predicate);
 	}
-	return data.filter((item): item is T => item != null);
 }
